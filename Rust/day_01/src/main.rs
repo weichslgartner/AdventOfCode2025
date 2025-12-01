ee0d1e29ec65a91b2c14aaff8e859737afc39dd4
weichslgartner <weichslgartner@gmail.com>
2025-12-01 13:11:58 +0100

    clean up

--- Rust/day_01/src/main.rs
+++ Rust/day_01/src/main.rs
@@ -1,31 +1,40 @@
 type Rotation = (char, i32);
 
 fn parse_input(lines: &str) -> Vec<Rotation> {
-    lines.lines()
-        .filter(|line| !line.is_empty()) 
+    lines
+        .lines()
+        .filter(|line| !line.is_empty())
         .map(|line| {
-            let op = line.chars().next().expect("Line must start with an operator");
-            let n = line[1..].parse::<i32>().expect("Rotation value must be an integer");            
+            let op = line
+                .chars()
+                .next()
+                .expect("Line must start with an operator");
+            let n = line[1..]
+                .parse::<i32>()
+                .expect("Rotation value must be an integer");
             (op, n)
         })
         .collect()
 }
 
-fn is_zero(numb: i32, _numb_old: i32) -> i32 {
-    if numb % 100 == 0 { 1 } else { 0 }
-}
-
-
-fn count_zero_crossings(numb: i32, numb_old: i32) -> i32 {
-    let mut cnt = 0;
-    if numb <= 0 && numb_old != 0 {
-        cnt += 1;
+fn is_zero(numb: i32, _numb_old: i32) -> u32 {
+    if numb % 100 == 0 {
+        1
+    } else {
+        0
     }
-    cnt += numb.abs() / 100;
-    cnt
 }
 
-fn solve(rotations: &[Rotation], condition: impl Fn(i32, i32) -> i32) -> i32 {
+fn count_zero_crossings(numb: i32, numb_old: i32) -> u32 {
+    let cnt: u32 = if (numb <= 0 && numb_old != 0) || (numb_old == 0 && numb == 0) {
+        1
+    } else {
+        0
+    };
+    cnt + numb.unsigned_abs() / 100
+}
+
+fn solve(rotations: &[Rotation], condition: impl Fn(i32, i32) -> u32) -> u32 {
     rotations
         .iter()
         // state holds the current modded value (starts at 50)
@@ -42,12 +51,11 @@ fn solve(rotations: &[Rotation], condition: impl Fn(i32, i32) -> i32) -> i32 {
         .sum()
 }
 
-
-fn part_1(rotations: &[Rotation]) -> i32 {
+fn part_1(rotations: &[Rotation]) -> u32 {
     solve(rotations, is_zero)
 }
 
-fn part_2(rotations: &[Rotation]) -> i32 {
+fn part_2(rotations: &[Rotation]) -> u32 {
     solve(rotations, count_zero_crossings)
 }
 
