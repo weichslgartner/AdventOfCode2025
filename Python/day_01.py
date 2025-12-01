a2bafeb8641a3dfd186f3514c3e83a877179d5e9
weichslgartner <weichslgartner@gmail.com>
2025-12-01 10:11:53 +0100

    clean up

--- Python/day_01.py
+++ Python/day_01.py
@@ -1,43 +1,53 @@
-from aoc import get_lines, line_to_int
+from operator import add, sub
+from functools import reduce
+from typing import List, Tuple, Callable
+
+from aoc import get_lines
+
+op_dict = {'R': add, 'L': sub}
 
 
-def parse_input(lines):
+def parse_input(lines: List[str]) -> List[Tuple[str, int]]:
     return [(line[0], int(line[1:])) for line in lines]
 
 
-def part_1(rotations):
-    pass
+def count_zeros(numb: int, _) -> int:
+    return numb % 100 == 0
 
 
-def solve(rotations):
-    cnt_a = 0
-    cnt_b = 0
+def count_zero_crossings(numb: int, numb_old: int) -> int:
+    cnt = 0
+    if numb <= 0 and numb_old != 0:
+        cnt += 1
+    cnt += abs(numb) // 100
+    return cnt
+
+
+def solve(rotations: List[Tuple[str, int]], condition: Callable[[int, int], int]) -> int:
+    cnt = 0
     numb = 50
-    numb_old = 50
+    numb_old = numb
     for r, n in rotations:
-        if r == 'L':
-            numb -= n
-        else:
-            numb += n
-        if numb <= 0 and numb_old != 0:
-            cnt_b += 1
-        cnt_b += abs(numb) // 100
+        numb = reduce(op_dict[r], [numb, n])
+        cnt += condition(numb, numb_old)
         numb %= 100
         numb_old = numb
-        cnt_a += 1 if numb == 0 else 0
-    return cnt_a, cnt_b
+    return cnt
 
 
-def part_2(lines):
-    pass
+def part_1(rotations: List[Tuple[str, int]]) -> int:
+    return solve(rotations, count_zeros)
+
+
+def part_2(rotations: List[Tuple[str, int]]) -> int:
+    return solve(rotations, count_zero_crossings)
 
 
 def main():
     lines = get_lines("input_01.txt")
     rotations = parse_input(lines)
-    cnt_a, cnt_b = solve(rotations)
-    print("Part 1:", cnt_a)
-    print("Part 2:", cnt_b)
+    print("Part 1:", part_1(rotations))
+    print("Part 2:", part_2(rotations))
 
 
 if __name__ == '__main__':
