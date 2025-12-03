42d8c77a556fa87ea319b07255750dcd80bd3f85
weichslgartner <weichslgartner@gmail.com>
2025-12-03 09:46:08 +0100

    day 03 refactor

--- Python/day_03.py
+++ Python/day_03.py
@@ -1,19 +1,25 @@
-from aoc import get_lines
+from typing import List, Dict, Optional
 from collections import defaultdict
+import heapq
+
+from aoc import get_lines
 
 
-def parse_input(lines):
+JoltDict = Dict[int, List[int]]
+
+
+def parse_input(lines: List[str]) -> List[JoltDict]:
     dics = []
     for line in lines:
         dic = defaultdict(list)
         for i, c in enumerate(line):
-            dic[int(c)].append(i)
-            dic[int(c)].sort()
+            heapq.heappush(dic[int(c)], i)
         dics.append(dict(sorted(dic.items(), reverse=True)))
     return dics
 
 
-def find_max_jolt_recursive(dic, current_number, used, length, acc):
+def find_max_jolt_recursive(dic: JoltDict, current_number: int, used: List[int], length: int, acc: int) \
+        -> Optional[int]:
     if length == 0:
         return acc + current_number
     for key, val in dic.items():
@@ -22,22 +28,21 @@ def find_max_jolt_recursive(dic, current_number, used, length, acc):
                 used.append(v)
                 if res := find_max_jolt_recursive(dic, key, used, length - 1, (acc + current_number) * 10):
                     return res
-                if used[-1] == v:
-                    del used[-1]
+                used.pop()
     return None
 
 
-def find_max_jolt(dic, length=1):
+def find_max_jolt(dic: JoltDict, length: int = 1) -> int:
     for k, v in dic.items():
         if res := find_max_jolt_recursive(dic, k, [v[0]], length, 0):
             return res
 
 
-def part_1(dics):
+def part_1(dics: List[JoltDict]) -> int:
     return sum(map(find_max_jolt, dics))
 
 
-def part_2(dics):
+def part_2(dics: List[JoltDict]) -> int:
     return sum(find_max_jolt(d, 11) for d in dics)
 
 
