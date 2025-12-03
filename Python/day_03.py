6d76f3733092a700a6e33a1d9bdf89267d5cc34b
weichslgartner <weichslgartner@gmail.com>
2025-12-03 11:34:48 +0100

    day 03 rust

--- Python/day_03.py
+++ Python/day_03.py
@@ -1,56 +1,54 @@
-from typing import List, Dict, Optional
 from collections import defaultdict
-import heapq
+from typing import List, Dict, Optional
 
 from aoc import get_lines
-
 
 JoltDict = Dict[int, List[int]]
 
 
 def parse_input(lines: List[str]) -> List[JoltDict]:
-    dics = []
+    battery_list = []
     for line in lines:
-        dic = defaultdict(list)
+        batteries = defaultdict(list)
         for i, c in enumerate(line):
-            heapq.heappush(dic[int(c)], i)
-        dics.append(dict(sorted(dic.items(), reverse=True)))
-    return dics
+            batteries[int(c)].append(i)
+        battery_list.append(dict(sorted(batteries.items(), reverse=True)))
+    return battery_list
 
 
-def find_max_jolt_recursive(dic: JoltDict, current_number: int, used: List[int], length: int, acc: int) \
+def find_max_jolt_recursive(batteries: JoltDict, current_number: int, used: List[int], length: int, acc: int) \
         -> Optional[int]:
     if length == 0:
         return acc + current_number
-    for key, val in dic.items():
+    for key, val in batteries.items():
         for v in val:
             if v > used[-1]:
                 used.append(v)
-                if res := find_max_jolt_recursive(dic, key, used, length - 1, (acc + current_number) * 10):
+                if res := find_max_jolt_recursive(batteries, key, used, length - 1, (acc + current_number) * 10):
                     return res
                 used.pop()
     return None
 
 
-def find_max_jolt(dic: JoltDict, length: int = 1) -> int:
-    for k, v in dic.items():
-        if res := find_max_jolt_recursive(dic, k, [v[0]], length, 0):
+def find_max_jolt(batteries: JoltDict, length: int = 1) -> int:
+    for k, v in batteries.items():
+        if res := find_max_jolt_recursive(batteries, k, [v[0]], length, 0):
             return res
 
 
-def part_1(dics: List[JoltDict]) -> int:
-    return sum(map(find_max_jolt, dics))
+def part_1(batteries: List[JoltDict]) -> int:
+    return sum(map(find_max_jolt, batteries))
 
 
-def part_2(dics: List[JoltDict]) -> int:
-    return sum(find_max_jolt(d, 11) for d in dics)
+def part_2(batteries: List[JoltDict]) -> int:
+    return sum(find_max_jolt(b, 11) for b in batteries)
 
 
 def main():
     lines = get_lines("input_03.txt")
-    dics = parse_input(lines)
-    print("Part 1:", part_1(dics))
-    print("Part 2:", part_2(dics))
+    batteries = parse_input(lines)
+    print("Part 1:", part_1(batteries))
+    print("Part 2:", part_2(batteries))
 
 
 if __name__ == '__main__':
---
+++ Rust/day_03/src/main.rs
@@ -0,0 +1,75 @@
+use std::collections::BTreeMap;
+type JoltDict = BTreeMap<i8, Vec<u32>>;
+
+fn parse_input(input: &str) -> Vec<JoltDict> {
+    let mut battery_list: Vec<JoltDict> = Vec::new();
+    for line in input.lines() {
+        let mut batteries: JoltDict = BTreeMap::new();
+        for (i, c) in line.chars().enumerate() {
+            if let Some(jolt) = c.to_digit(10) {
+                batteries.entry(jolt as i8).or_default().push(i as u32);
+                batteries.get_mut(&(jolt as i8)).unwrap().sort_unstable();
+            }
+        }
+        battery_list.push(batteries);
+    }
+    battery_list
+}
+
+fn find_max_jolt_recursive(
+    batteries: &JoltDict,
+    current_number: u64,
+    used: &mut Vec<u32>,
+    length: u32,
+    acc: u64,
+) -> Option<u64> {
+    if length == 0 {
+        return Some(acc + current_number);
+    }
+    for key in batteries.keys().rev() {
+        if let Some(val) = batteries.get(key) {
+            for &v in val {
+                if v > *used.last().unwrap() {
+                    used.push(v);
+                    let new_acc = (acc + current_number) * 10u64;
+                    if let Some(res) =
+                        find_max_jolt_recursive(batteries, *key as u64, used, length - 1, new_acc)
+                    {
+                        return Some(res);
+                    }
+                    used.pop();
+                }
+            }
+        }
+    }
+    None
+}
+
+fn find_max_jolt(batteries: &JoltDict, length: u32) -> u64 {
+    for k in batteries.keys().rev() {
+        if let Some(v) = batteries.get(k) {
+            let mut used = vec![v.first().cloned().unwrap()];
+            if let Some(res) =
+                find_max_jolt_recursive(batteries, *k as u64, &mut used, length - 1, 0u64)
+            {
+                return res;
+            }
+        }
+    }
+    0u64
+}
+
+fn part_1(batteries: &[JoltDict]) -> u64 {
+    batteries.iter().map(|b| find_max_jolt(b, 2)).sum::<u64>()
+}
+
+fn part_2(batteries: &[JoltDict]) -> u64 {
+    batteries.iter().map(|b| find_max_jolt(b, 12)).sum::<u64>()
+}
+
+fn main() {
+    let input = include_str!("../../../inputs/input_03.txt");
+    let batteries = parse_input(input);
+    println!("Part 1: {}", part_1(&batteries));
+    println!("Part 2: {}", part_2(&batteries));
+}
