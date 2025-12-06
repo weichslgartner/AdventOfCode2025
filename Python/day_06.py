3e3ac7ccc5186abdf0c31f2516fab1a8cf832161
weichslgartner <weichslgartner@gmail.com>
2025-12-06 16:39:20 +0100

    day 06 python refactor

--- Python/day_06.py
+++ Python/day_06.py
@@ -1,40 +1,43 @@
 from functools import reduce
+from operator import add, imul
+from typing import List, Callable, Tuple
 
 from aoc import get_lines
-from operator import add, imul
+
+OperatorFunc = Callable[[int, int], int]
 
 
-def parse_input(lines):
+def parse_input(lines: List[str]) -> Tuple[List[List[int]], List[List[int]], List[OperatorFunc]]:
     ops = {"*": imul, "+": add}
-    numbers_part1 = [[] for _ in range(len(lines[0].split()))]
+    split_lines = [line.split() for line in lines[:-1]]
+    numbers_part1 = [[int(cols[i]) for cols in split_lines]for i in range(len(split_lines[0]))]
     numbers_part2 = []
     cur_list = []
-    for idx in range(len(lines[0])-1,-1,-1):
+    for idx in range(len(lines[0]) - 1, -1, -1):
         cur = 0
-        for l in lines[:-1]:
-            if l[idx] != " ":
-                cur = cur*10 + int(l[idx])
+        for line in lines[:-1]:
+            if line[idx] != " ":
+                cur = cur * 10 + int(line[idx])
         if cur != 0:
             cur_list.append(cur)
         else:
             numbers_part2.append(cur_list)
             cur_list = []
     numbers_part2.append(cur_list)
-    [[numbers_part1[i].append(int(c)) for i, c in enumerate(line.split())] for line in lines[:-1]],
-    return numbers_part1,numbers_part2, [ops[c] for c in lines[-1].split()]
+    return numbers_part1, numbers_part2, [ops[c] for c in lines[-1].split()]
 
 
-def part_1(numbers, operators):
+def part_1(numbers: List[List[int]], operators: List[OperatorFunc]) -> int:
     return sum(reduce(operators[i], n) for i, n in enumerate(numbers))
 
 
-def part_2(numbers, operators):
-    return sum(reduce(operators[len(operators)-1-i], n) for i, n in enumerate(numbers))
+def part_2(numbers: List[List[int]], operators: List[OperatorFunc]) -> int:
+    return sum(reduce(operators[len(operators) - 1 - i], n) for i, n in enumerate(numbers))
 
 
-def main():
+def main() -> None:
     lines = get_lines("input_06.txt")
-    numbers_part1,numbers_part2, operators = parse_input(lines)
+    numbers_part1, numbers_part2, operators = parse_input(lines)
     print("Part 1:", part_1(numbers_part1, operators))
     print("Part 2:", part_2(numbers_part2, operators))
 
--- get_description.py
+++ get_description.py
@@ -2,10 +2,10 @@ from requests import get
 from pathlib import Path
 from markdownify import markdownify as md
 
-year=2025
+year = 2025
 
 cookie = open(".cookie", 'r').readline().strip()
 cookies = {'session': cookie}
-for day in range(1,26):
+for day in range(1, 26):
     raw_input = get(f"https://adventofcode.com/{year}/day/{day}", cookies=cookies)
     Path(f"tasks/{day:02d}.md").open("w").write(md(raw_input.content))
