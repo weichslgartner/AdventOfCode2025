c615f3abde4a4d35dfd130da147b76c57ad8fde2
Andreas Weichslgartner <andreasw@rivianvw.tech>
2025-12-09 10:51:24 +0100

    day_09 part2

--- Python/day_09.py
+++ Python/day_09.py
@@ -7,16 +7,65 @@ def parse_input(lines):
 
 def part_1(points):
     max_area = 0
-    #p_list  = sorted([(p.x * p.y,p) for p in points])
-    #print(p_list)
     for p1, p2 in combinations(points, 2):
         area = (abs(p1.x - p2.x)+1) * (abs(p1.y - p2.y)+1)
         max_area = max(max_area, area)
     return max_area
 
 def part_2(points):
-    pass
+    lines = []
+    for p1, p2 in zip(points, points[1:]):
+        lines.append((p1, p2))
+    lines.append((points[-1], points[0]))
+    import matplotlib.pyplot as plt
+    points.append(points[0])  # to create a closed loop
+    xs, ys = zip(*points) #create lists of x and y values
 
+    #plt.figure()
+    #plt.plot(xs,ys) 
+    #plt.show()
+    #94651,50319
+    #94651,48450
+    max_area = 0
+    best = None
+    p1 = Point(94651,50319)
+    for p2 in points: 
+        if p1 == p2 or p2.y < p1.y:
+            continue  
+        to_check =Point(p1.x,p2.y)
+        lines_to_check = []
+        for line in lines:
+           if line[0] == p1 or line[1] == p1:
+               continue
+           if (min(line[0].x, line[1].x) <= to_check.x <= max(line[0].x, line[1].x)) and line[0].y >= p1.y:
+                lines_to_check.append(line)
+        if all(to_check.y <= min(line[0].y,line[1].y) for line in lines_to_check):
+            area = (abs(p1.x - p2.x)+1) * (abs(p1.y - p2.y)+1)
+            if area > max_area:
+                max_area = area
+                best = [p1, Point(p1.x, p2.y), p2, Point(p2.x, p1.y), p1]
+    p1 = Point(94651,48450)
+    lines_to_check = []
+    for p2 in points: 
+        if p1 == p2 or p2.y > p1.y:
+            continue  
+        to_check =Point(p1.x,p2.y)
+        for line in lines:
+           if line[0] == p1 or line[1] == p1:
+               continue
+           if (min(line[0].x, line[1].x) <= to_check.x <= max(line[0].x, line[1].x)) and line[0].y <= p1.y:
+                lines_to_check.append(line)
+        if all(to_check.y >= max(line[0].y,line[1].y) for line in lines_to_check):
+            area = (abs(p1.x - p2.x)+1) * (abs(p1.y - p2.y)+1)
+            if area > max_area:
+                max_area = area
+                best = [p1, Point(p1.x, p2.y), p2, Point(p2.x, p1.y), p1]
+   # plt.figure()
+   # plt.plot(xs,ys) 
+   # if best:
+   #     plt.plot(*zip(*best)) 
+   # plt.show()
+    return max_area
 
 def main():
     lines = get_lines("input_09.txt")
