f26ff9e64ffe284206f6e0c97d8fbba8f9b325cc
Andreas Weichslgartner <andreasw@rivianvw.tech>
2025-12-10 11:37:41 +0100

    day 10 part 2

--- Python/day_10.py
+++ Python/day_10.py
@@ -1,11 +1,11 @@
 from collections import defaultdict
-from copy import copy
-import random
 import sys
 from aoc import get_lines
 from functools import cache
 import sys
-sys.setrecursionlimit(100000) 
+
+sys.setrecursionlimit(100000)
+
 
 def parse_input(lines):
     lights_diagram = []
@@ -15,7 +15,7 @@ def parse_input(lines):
         wiring = []
         for token in line.split():
             if "[" in token and "]" in token:
-                lights_diagram.append(tuple( x=="#" for x in token[1:-1]))
+                lights_diagram.append(tuple(x == "#" for x in token[1:-1]))
             elif "(" in token:
                 wiring.append([int(x) for x in token[1:-1].split(",")])
             else:
@@ -23,33 +23,40 @@ def parse_input(lines):
                 joltage_reqs.append([int(x) for x in token[1:-1].split(",")])
     return lights_diagram, wirings, joltage_reqs
 
-def is_same(w1,w2):
-    for x,y in zip(w1,w2):
-        if x!=y:
+
+def is_same(w1, w2):
+    for x, y in zip(w1, w2):
+        if x != y:
             return False
     return True
 
-def need_change(state,target):
+
+def need_change(state, target):
     change = []
-    for i, el in enumerate(zip(state,target)):
+    for i, el in enumerate(zip(state, target)):
         change.append(i)
     return change
 
-def update_state(state,wiring):
-    return tuple(not s if i in wiring else s for i,s in enumerate(state))
+
+def update_state(state, wiring):
+    return tuple(not s if i in wiring else s for i, s in enumerate(state))
+
+
+def update_state_partb(state, wiring):
+    state = list(state)
+    for w in wiring:
+        state[w] += 1
+    return tuple(state)
+
 
 def part_1(lights_diagram, wirings):
-    #print(lights_diagram,wirings)
+    # print(lights_diagram,wirings)
     overall = 0
 
-    for lights,wiring in zip(lights_diagram, wirings):
-        w_dict = defaultdict(list)
-        for w in wiring:
-            for x in w:
-                w_dict[x].append(w)
-        #print(w_dict)
+    for lights, wiring in zip(lights_diagram, wirings):
         best = sys.maxsize
         state_to_presses = dict()
+
         @cache
         def dfs(state, target, b_presses):
             if is_same(state, target):
@@ -65,24 +72,76 @@ def part_1(lights_diagram, wirings):
                 new_state = update_state(state, w)
                 dfs(new_state, target, b_presses + 1)
             return
-        print(lights)
+
+        #print(lights)
         dfs(tuple(False for _ in lights), lights, 0)
+
+        #print(best)
+        overall += best
+    return overall
+
+
+def part_2_req(wirings, joltage_reqs):
+    overall = 0
+    for wiring, joltage_req in zip(wirings, joltage_reqs):
+        best = sys.maxsize
+
+        @cache
+        def dfs(state, target, b_presses):
+            if is_same(state, target):
+                nonlocal best
+                best = min(best, b_presses)
+                return
+            for s, t in zip(state, target):
+                if s > t:
+                    return
+            if b_presses >= best:
+                return
+            for w in wiring:
+                new_state = update_state_partb(state, w)
+                dfs(new_state, target, b_presses + 1)
+            return
+
+        print(lights)
+        dfs(tuple(0 for _ in joltage_req), tuple(i for i in joltage_req), 0)
 
         print(best)
         overall += best
     return overall
 
 
-def part_2(lines):
-    pass
+def part_2(wirings, joltage_reqs):
+    from z3 import Int, Optimize, sat
+
+    overall = 0
+    for wiring, joltage_req in zip(wirings, joltage_reqs):
+        cs = []
+        xs = []
+        for i, wire in enumerate(wiring):
+            c = [1 if i in wire else 0 for i in range(len(joltage_req))]
+            cs.append(c)
+            xs.append(Int(f"x{i}"))
+        d = joltage_req
+        solver = Optimize()
+        for i in range(len(d)):
+            solver.add(sum(xs[j] * cs[j][i] for j in range(len(xs))) == d[i])
+        solver.add([xs[i] >= 0 for i in range(len(xs))])
+        solver.minimize(sum(xs))
+        if solver.check() == sat:
+            m = solver.model()
+            total_presses = sum(m.evaluate(xs[i]).as_long() for i in range(len(xs)))
+            overall += total_presses
+        else:
+            print("No solution")
+    return overall
 
 
 def main():
     lines = get_lines("input_10.txt")
     lights_diagram, wirings, joltage_reqs = parse_input(lines)
     print("Part 1:", part_1(lights_diagram, wirings))
-    print("Part 2:", part_2(lines))
+    print("Part 2:", part_2(wirings, joltage_reqs))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
